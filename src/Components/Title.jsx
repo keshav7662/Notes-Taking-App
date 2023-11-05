c0cc3c08b2cebe6525e8c286277f726459dc22f9
Keshav Kumar <104895178+BitcodeVelocity@users.noreply.github.com>
2023-11-05 21:27:17 +0530

    Added title component!

---
+++ src/Components/Title.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import styles from './Title.module.css'
+const Title = ({ titleData,selectedTitle,setSelectedTitle, setDisplayPoster }) => {
+
+  const shortName = titleData.name.substring(0, 2).toUpperCase();
+  const fullName = titleData.name;
+  const backgroundColor = titleData.color;
+  
+  const setTitle = () => {
+    setSelectedTitle(titleData);
+    setDisplayPoster(false);
+  }
+  const selected = titleData === selectedTitle ? '#F7ECDC':''
+  console.log(titleData)
+  console.log(selectedTitle)
+  return (
+    <>
+      <div className={styles.titles} onClick={setTitle} style={{backgroundColor:selected}}>
+        <div className={styles['short-name']} style={{ backgroundColor }}>{shortName}</div>
+        <div className={styles['full-name']}><h4>{fullName}</h4></div>
+      </div>
+
+    </>
+  )
+}
+
+export default Title
